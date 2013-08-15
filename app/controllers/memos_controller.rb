6dbd2df0086addea17471c41fd7e143225fe84da
moritamoriko <hato3000wa@gmail.com>
2013-08-15 20:54:37 +0900

    entryの値がnill

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,4 +1,20 @@
 class MemosController < ApplicationController
 	def new
+      @memo = Memo.new
+
+      respond_to do |format|
+        format.html # new.html.erb
+        format.json { render json: @memo }
+      end
+
 	end
+	def create
+		@memo = Memo.new(params[:entry])
+		if @memo.save
+			redirect_to new_memo_path
+		else
+			redirect_to root_path
+		end
+	end	
+
 end
